b049216fe9860c9ba95792508af371e866932104
Guilherme Bruzzi <guibruzzi@gmail.com>
2021-05-07 18:03:04 -0300

    Initial commit

---
+++ react/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import type { FC } from 'react'
+
+const InstoreComponent: FC = () => {
+  return <div>Hello World from inStore component</div>
+}
+
+export default InstoreComponent
